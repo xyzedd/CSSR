8f096a98b2df82fd6ec2c2548c1f350987963022
lakpa-tamang9 <Ld.tamang25@gmail.com>
2023-03-14 11:55:55 +0900

    refactor: update backbone net of wideresnet

--- methods/wideresnet.py
+++ methods/wideresnet.py
@@ -5,6 +5,18 @@ import torch.nn.functional as F
 
 
 class BasicBlock(nn.Module):
+    """The residual network consists of sequentially stacked residual blocks \
+        https://arxiv.org/pdf/1603.05027.pdf
+        - Consists of two type of blocks: basic, bottleneck
+        - This is the class of basic block where two consecutive 3 x 3 \
+            convolution with batch norm and ReLU preceding convolution.
+
+        - The BN-ReLU-Conv was shown to trin faster and achieve better results\
+            than the original version which is conv-BN-ReLU
+    Args:
+        nn (_type_): _description_
+    """
+
     def __init__(self, in_planes, out_planes, stride, dropRate=0.0, activate_before_residual=False):
         super(BasicBlock, self).__init__()
         self.bn1 = nn.BatchNorm2d(in_planes, momentum=0.001)
@@ -18,8 +30,9 @@ class BasicBlock(nn.Module):
         self.droprate = dropRate
         self.equalInOut = (in_planes == out_planes)
         self.convShortcut = (not self.equalInOut) and nn.Conv2d(in_planes, out_planes, kernel_size=1, stride=stride,
-                               padding=0, bias=False) or None
+                                                                padding=0, bias=False) or None
         self.activate_before_residual = activate_before_residual
+
     def forward(self, x):
         if not self.equalInOut and self.activate_before_residual == True:
             x = self.relu1(self.bn1(x))
@@ -31,22 +44,42 @@ class BasicBlock(nn.Module):
         out = self.conv2(out)
         return torch.add(x if self.equalInOut else self.convShortcut(x), out)
 
+
 class NetworkBlock(nn.Module):
+    """Block used in wideresnet.
+    nb_layers is a factor that scales the width of the residual block.
+
+    Args:
+        nn (_type_): _description_
+    """
+
     def __init__(self, nb_layers, in_planes, out_planes, block, stride, dropRate=0.0, activate_before_residual=False):
         super(NetworkBlock, self).__init__()
-        self.layer = self._make_layer(block, in_planes, out_planes, nb_layers, stride, dropRate, activate_before_residual)
+        self.layer = self._make_layer(
+            block, in_planes, out_planes, nb_layers, stride, dropRate, activate_before_residual)
+
     def _make_layer(self, block, in_planes, out_planes, nb_layers, stride, dropRate, activate_before_residual):
         layers = []
         for i in range(int(nb_layers)):
-            layers.append(block(i == 0 and in_planes or out_planes, out_planes, i == 0 and stride or 1, dropRate, activate_before_residual))
+            layers.append(block(i == 0 and in_planes or out_planes, out_planes,
+                          i == 0 and stride or 1, dropRate, activate_before_residual))
         return nn.Sequential(*layers)
+
     def forward(self, x):
         return self.layer(x)
 
 
-
 class WideResNetBackbone(nn.Module):
-    def __init__(self, args, depth=28, widen_factor=2, dropRate=0.0,req_output_dim = -1):
+    """In wideresnet only basic block is used and the bottleneck network is not used.\
+        The bottleneck is used to make networks thinner.
+
+        Network block is a block that incorporates the width of the width of the network
+
+    Args:
+        nn (_type_): _description_
+    """
+
+    def __init__(self, args, depth=28, widen_factor=2, dropRate=0.0, req_output_dim=-1):
         super(WideResNetBackbone, self).__init__()
         nChannels = [16, 16*widen_factor, 32*widen_factor, 64*widen_factor]
         assert((depth - 4) % 6 == 0)
@@ -56,11 +89,14 @@ class WideResNetBackbone(nn.Module):
         self.conv1 = nn.Conv2d(3, nChannels[0], kernel_size=3, stride=1,
                                padding=1, bias=False)
         # 1st block
-        self.block1 = NetworkBlock(n, nChannels[0], nChannels[1], block, 1, dropRate, activate_before_residual=True)
+        self.block1 = NetworkBlock(
+            n, nChannels[0], nChannels[1], block, 1, dropRate, activate_before_residual=True)
         # 2nd block
-        self.block2 = NetworkBlock(n, nChannels[1], nChannels[2], block, 2, dropRate)
+        self.block2 = NetworkBlock(
+            n, nChannels[1], nChannels[2], block, 2, dropRate)
         # 3rd block
-        self.block3 = NetworkBlock(n, nChannels[2], nChannels[3], block, 2, dropRate)
+        self.block3 = NetworkBlock(
+            n, nChannels[2], nChannels[3], block, 2, dropRate)
         # global average pooling and classifier
         self.bn1 = nn.BatchNorm2d(nChannels[3], momentum=0.001)
         self.relu = nn.LeakyReLU(negative_slope=0.1, inplace=True)
@@ -68,13 +104,14 @@ class WideResNetBackbone(nn.Module):
         self.output_dim = nChannels[3]
         if req_output_dim > 0 and req_output_dim != self.output_dim:
             self.dim_map = nn.Sequential(
-                nn.Conv2d(self.output_dim, req_output_dim, 1,1, 0, bias=False),
+                nn.Conv2d(self.output_dim, req_output_dim,
+                          1, 1, 0, bias=False),
                 nn.BatchNorm2d(req_output_dim),
                 nn.LeakyReLU(0.2))
             self.output_dim = req_output_dim
         else:
             self.dim_map = None
-        
+
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
@@ -95,5 +132,3 @@ class WideResNetBackbone(nn.Module):
         if self.dim_map is not None:
             out = self.dim_map(out)
         return out
-
-
